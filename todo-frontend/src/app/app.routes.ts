a5f69e2d8d500a81590bb2bcca4bf053af6a3315
Pablo-Devs <blanksonpaul3@gmail.com>
2025-02-23 22:38:18 +0000

    feat: configure routing

--- todo-frontend/src/app/app.routes.ts
+++ todo-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', title: 'Signin', component: SigninComponent },
+];
